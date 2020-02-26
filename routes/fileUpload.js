42fb416092e8e28a3b9d3d0a43b371e40144950e
Raheel1258 <rahilahmad1258@gmail.com>
2020-02-27 04:38:26 +0500

    make api to upload image

---
+++ routes/fileUpload.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const upload = require('../services/fileUpload');
+const singleUpload = upload.single('image');
+
+router.post('/image-upload', function(req, res) {
+    singleUpload(req, res, function(err){
+       if (err){
+           return res.status(400).send({errors: {tittle: "File upload error", detail: err.message}})
+       }        
+        return res.json({'imageUrl': req.file.location});
+    })
+});
+
+module.exports = router;
