c804722c02834de13f7a9d189f54c25586a5d26c
Raheel Ahmed <rahilahmad1258@gmail.com>
2020-02-26 23:41:54 -0800

    changes

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,61 +1,97 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const UserModel = require("../models/user");
 
-const UserModel = require('../models/user');
-
-const aws = require('aws-sdk')
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 
 aws.config.update({
-    accessKeyId: 'AKIAJBJ57PPJ54T2BREA',
-    secretAccessKey: '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',
-    region: 'us-east-2'
+  accessKeyId: "AKIAJBJ57PPJ54T2BREA",
+  secretAccessKey: "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",
+  region: "us-east-2"
 });
 
 const s3 = new aws.S3();
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype === null){
-      cb(null, true)
-    } else{
-      cb(new Error('Invalid image type, only JPEG and png are allowed' ), false);
-    }
-}
+  if (
+    file.mimetype == "image/jpeg" ||
+    file.mimetype == "image/png" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype === null
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid image type, only JPEG and png are allowed"), false);
+  }
+};
 
 const upload = multer({
-    fileFilter,
-    storage: multerS3({
+  fileFilter,
+  storage: multerS3({
     s3: s3,
-    bucket: 'aptake',
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: "TESTING_META_DATA"});
+    bucket: "aptake",
+    acl: "public-read",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: "TESTING_META_DATA" });
     },
-    key: function (req, file, cb) {
-      console.log(req.params.user_id);
-      cb(null,"images/user_id/" + file.originalname, Date.now().toString())
+    key: function(req, file, cb) {
+      console.log(file);
+      cb(
+        null,
+        `images/${req.params.user_id}/` + file.originalname,
+        Date.now().toString()
+      );
     }
   })
 });
 
-const singleUpload = upload.single('image');
+const singleUpload = upload.single("image");
 
-router.post('/imageUpload/:user_id', function(req, res) {
-    
-    var user = req.params;
-    singleUpload(req, res, function(err){
-        console.log("Salman Saleem " + req.file.location)
-       if (err){
-           return res.status(400).send({errors: {tittle: "File upload error", detail: err.message}})
-       } 
-       UserModel.update({profile_pic: req.file.location}, {
-           where: {
-            identity_no: user.user_id
-           }
-       })
-        return res.json({'imageUrl': req.file.location});
-    })
+router.post("/imageUpload/:user_id", function(req, res) {
+  singleUpload(req, res, async function(err) {
+    console.log("Salman Saleem " + req.file);
+    if (err) {
+      return res.status(400).send({
+        errors: { message: "File upload error", detail: err.message }
+      });
+    }
+
+    const updated_profile = await new Promise((resolve, reject) => {
+      UserModel.findOneAndUpdate(
+        { _id: req.params.user_id },
+        {
+          $set: {
+            profile_pic: req.file.location
+          }
+        },
+        { new: true },
+        (err, data) => {
+          if (!err) {
+            resolve(data);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+    if (updated_profile) {
+      res.status(200).send({
+        status: true,
+        message: "Image uploaded successfuly",
+        data: updated_profile,
+        imageUrl: req.file.location
+      });
+    } else {
+      res.status(401).send({
+        status: false,
+        message: "Unable to update profile picture",
+        data: {}
+      });
+    }
+  });
 });
 
 module.exports = router;
