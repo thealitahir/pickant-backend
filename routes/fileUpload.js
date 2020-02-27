9242a17fabf3810be83ec2e77d351a9046c83bb0
Raheel1258 <rahilahmad1258@gmail.com>
2020-02-27 05:10:44 +0500

    update imageupload api route

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
 
 const upload = require('../services/fileUpload');
 const singleUpload = upload.single('image');
 
-router.post('/image-upload', function(req, res) {
+router.post('/imageUpload', function(req, res) {
     singleUpload(req, res, function(err){
        if (err){
            return res.status(400).send({errors: {tittle: "File upload error", detail: err.message}})
